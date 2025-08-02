aaa78b13c815a5bac1a7bc2722876688c14bfb15
GreedyGaurav <gauravpinocchio786@gmail.com>
2025-08-02 14:03:49 +0530

    Fix profile button to redirect to settings page

--- components/dashboard-layout.tsx
+++ components/dashboard-layout.tsx
@@ -111,7 +111,7 @@ export function DashboardLayout({ children }: { children: React.ReactNode }) {
                         )}
                       </div>
                     </div>
-                    <DropdownMenuItem onClick={() => handleNavigation("/profile")}>
+                    <DropdownMenuItem onClick={() => handleNavigation("/settings")}>
                       <User className="mr-2 h-4 w-4" />
                       <span>Profile</span>
                     </DropdownMenuItem>
